1aa3bfc6d0dc2bc11bd48bd835b1b09b623eec63
Felix Hao <felixhao28@gmail.com>
2017-07-07 10:29:46 +0800

    add .net core version

---
+++ src/DotnetCore.Segmenter.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using JiebaNet.Segmenter;
+using System;
+
+namespace DotnetCore.Segmenter.Cli
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var segmenter = new JiebaSegmenter();
+            var r = segmenter.Cut("大家好，我叫小娜。");
+            Console.WriteLine(String.Join(',', r));
+        }
+    }
+}
